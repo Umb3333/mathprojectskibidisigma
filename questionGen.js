73e152c8550a625b03d7b8739df659719e793898
Umb3333 <148855458+Umb3333@users.noreply.github.com>
2024-11-05 17:03:17 +0100

    meow

--- questionGen.js
+++ questionGen.js
@@ -1,22 +1,19 @@
 
 
-document.getElementsByClassName("question")[0].innerHTML = question[0];
-document.getElementsByClassName("questionnumber")[0].innerHTML = "Question 1";
-document.getElementsByClassName("input1")[0].placeholder = "Answer";
-
 function randomArithmetic() {
-    const operations = ['+', '-', '*', '/'];
+    const operations = ["+", "-", "*", "/"];
     const randomIndex = Math.floor(Math.random() * operations.length);
     return operations[randomIndex];
   }
   function questionGen() {
-    const num1 = Math.random(1, 1000);
-    const num2 = Math.random(1, 1000);
+    const num1 = Math.floor(Math.random() * 1000);
+    const num2 = Math.floor(Math.random() * 1000);
     const operation = randomArithmetic();
     const question = `What is ${num1} ${operation} ${num2}?`;
     const answer = eval(`${num1} ${operation} ${num2}`);
-    console.log(question);
-    console.log(`Answer: ${answer}`);
+
+    document.querySelector(".question").textContent = question;
+    
   }
 
-  questionGen();
+  
