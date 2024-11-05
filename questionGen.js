83ab0a32866b2785726a1fd7548aedcd8cc1befa
Umb3333 <148855458+Umb3333@users.noreply.github.com>
2024-11-05 17:15:31 +0100

    balls

--- questionGen.js
+++ questionGen.js
@@ -1,13 +1,24 @@
+// Emanuel Ferris wrote this code :3
 
+const input = document.querySelector(".input1");
 
 function randomArithmetic() {
+  // this function randomly selects an operation from the array.
     const operations = ["+", "-", "*", "/"];
+    //                  Rounds down ||  Random number || The amount of operations listed.
     const randomIndex = Math.floor(Math.random() * operations.length);
     return operations[randomIndex];
   }
+
+  function addMoreToProblem(){
+
+  }
+
   function questionGen() {
     const num1 = Math.floor(Math.random() * 1000);
     const num2 = Math.floor(Math.random() * 1000);
+    const num3 = Math.floor(Math.random() * 1000);
+    
     const operation = randomArithmetic();
     const question = `What is ${num1} ${operation} ${num2}?`;
     const answer = eval(`${num1} ${operation} ${num2}`);
