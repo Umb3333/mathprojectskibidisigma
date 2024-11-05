c80672206389e61505dcf2b25bfd5e6765bd4058
Umb3333 <148855458+Umb3333@users.noreply.github.com>
2024-11-05 17:49:55 +0100

    wobble

--- questionGen.js
+++ questionGen.js
@@ -1,6 +1,8 @@
 // Emanuel Ferris wrote this code :3
 
 const input = document.querySelector(".input1");
+const submit = document.querySelector(".submit");
+const questionnumber = document.querySelector(".questionnumber");
 
 function randomArithmetic() {
   // this function randomly selects an operation from the array.
@@ -10,17 +12,17 @@ function randomArithmetic() {
     return operations[randomIndex];
   }
 
-  function addMoreToProblem(){
-
-  }
+  //function addMoreToProblem(){}
 
   function questionGen() {
+    // this part generates the numbers for the problem.
     const num1 = Math.floor(Math.random() * 1000);
     const num2 = Math.floor(Math.random() * 1000);
     const num3 = Math.floor(Math.random() * 1000);
     
     const operation = randomArithmetic();
     const question = `What is ${num1} ${operation} ${num2}?`;
+    //  Holds the answer to the question. (duh)
     const answer = eval(`${num1} ${operation} ${num2}`);
 
     document.querySelector(".question").textContent = question;
