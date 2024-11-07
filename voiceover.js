cc675dde38a697495c645fe1357491156c92349b
Herroeverynyan <leonard.kampe@elev.ga.lbs.se>
2024-11-07 13:04:25 +0100

    mmm balls
    balls

--- voiceover.js
+++ voiceover.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { // skriven av Leo
     const voiceoverToggle = document.getElementById('voiceover-toggle');
 
     
